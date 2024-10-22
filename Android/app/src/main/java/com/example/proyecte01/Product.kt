07595b10eb116feb187a0d852c08594381130af4
a23brioropoy <a23brioropoy@inspedralbes.cat>
2024-10-22 17:04:12 +0200

    Add files via upload
    Ver productos y carrito

---
+++ Android/app/src/main/java/com/example/proyecte01/Product.kt
@@ -0,0 +1,13 @@
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Product(
+    val product_id: Int,
+    val product_name: String,
+    val description: String,
+    val price: Double,
+    val stock: Int,
+    val image_file: String,
+    val active: Boolean
+) : Parcelable
